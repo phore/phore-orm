fcdb4559629604666764df54c27f4039aaa838ed
Matthias <m@tth.es>
2026-04-21 14:45:56 +0200

    sync

--- src/Exception/SqlSyntaxException.php
+++ src/Exception/SqlSyntaxException.php
@@ -4,13 +4,10 @@ namespace Phore\MiniSql\Exception;
 
 class SqlSyntaxException extends OrmQueryException
 {
-    public function __construct(string $message = "", public readonly string $sqlStmt = "", int $code = 0, ?Throwable $previous = null)
+    public function __construct(string $message = "", public readonly string $sqlStmt = "", int $code = 0, ?\Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
     
-    public function getMessage() {
-        return $this->message . " on sql stmt: " . $this->sqlStmt;
-    }
     
 }
